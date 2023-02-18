6364cd8b09a7a33e5871d8bc38c23f18a4792bfe
padhiarigithub <111277991+padhiarigithub@users.noreply.github.com>
2023-02-18 16:41:41 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
  region="ap-southeast-1"
-  access_key = "AKIASBOQXMAZQAIYUK2M"
- secret_key = "o3ISwPkeh2C18rmz7K+6StZeCJPQwdWjVPjdlB1l"
 }
 #terraform script for ec2 instance
 resource "aws_instance" "example" {
